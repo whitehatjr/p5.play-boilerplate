b21b5946a7713af341a62ebe3b24df8ae6189902
Ekaksh794 <72455542+Ekaksh794@users.noreply.github.com>
2020-12-07 22:50:28 +0530

    Create plinko.js

---
+++ plinko.js
@@ -0,0 +1,21 @@
+function Plinko(x, y, r) {
+  var options = {
+    restitution: 1,
+    friction: 0,
+    isStatic: true
+  };
+  this.body = Bodies.circle(x, y, r, options);
+  this.body.label = 'plinko';
+  this.r = r;
+  World.add(world, this.body);
+}
+
+Plinko.prototype.show = function() {
+  noStroke();
+  fill(127);
+  var pos = this.body.position;
+  push();
+  translate(pos.x, pos.y);
+  ellipse(0, 0, this.r * 2);
+  pop();
+};
